dde0133aa873c508524b1b05e0d6efce604f9837
learnlongway777 <961300415@qq.com>
2020-07-20 01:27:41 +0800

    Basic model finished

--- app/src/main/java/com/pp/neteasemusic/MainActivity.java
+++ app/src/main/java/com/pp/neteasemusic/MainActivity.java
@@ -9,15 +9,19 @@ import android.os.Bundle;
 import android.os.IBinder;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
-import com.google.android.material.tabs.TabLayout;
-import com.pp.neteasemusic.netease.notification.NeteaseNotification;
+import com.google.android.material.navigation.NavigationView;
 import com.pp.neteasemusic.netease.receiver.NetEaseReceiver;
 import com.pp.neteasemusic.netease.room.RoomManager;
 import com.pp.neteasemusic.netease.service.MusicService;
 
 public class MainActivity extends AppCompatActivity {
+    NavController controller;
+    AppBarConfiguration configuration;
     IMusicController musicController;
     NetEaseReceiver receiver;
     IntentFilter intentFilter;
@@ -37,28 +41,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RoomManager.initRoomManager(getApplicationContext());
+        RoomManager.setDisplay(getWindowManager().getDefaultDisplay());
+        //注册音乐管理服务
         Intent intent = new Intent("com.pp.neteasemusic.MusicService");
         intent.setComponent(new ComponentName(getApplicationContext(), MusicService.class));
         bindService(intent, connection, Context.BIND_AUTO_CREATE);
-
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
-
-        RoomManager.initRoomManager(getApplicationContext());
-        RoomManager.setViewPager(viewPager);
-        RoomManager.setDisplay(getWindowManager().getDefaultDisplay());
-
-        NeteaseNotification.initInstance();
+        //注册通知服务
         receiver = new NetEaseReceiver();
         intentFilter = new IntentFilter();
         intentFilter.addAction(NetEaseReceiver.NOTIFICATION_ITEM_BUTTON_PRE);
         intentFilter.addAction(NetEaseReceiver.NOTIFICATION_ITEM_BUTTON_PLAY);
         intentFilter.addAction(NetEaseReceiver.NOTIFICATION_ITEM_BUTTON_NEXT);
         registerReceiver(receiver, intentFilter);
-
+        //获取状态栏高度
         int height = 0;
         int resourceId = getApplicationContext().getResources().getIdentifier("status_bar_height", "dimen", "android");
         if (resourceId > 0) {
@@ -67,6 +64,18 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("Status bar height-----------");
         System.out.println(height);
 
+        controller = Navigation.findNavController(this, R.id.fragment);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        configuration = new AppBarConfiguration.Builder(navigationView.getMenu()).build();
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//        NavigationUI.setupActionBarWithNavController(this,controller,configuration);
+        NavigationUI.setupWithNavController(navigationView, controller);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return super.onSupportNavigateUp();
     }
 
     @Override
